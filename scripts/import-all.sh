e50875fe08dc75d3726f01f7403c890b7665801d
Andrey Popp <8mayday@gmail.com>
2023-10-25 20:02:53 +0400

    mlx.0.3

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -1,4 +1,6 @@
 ./scripts/import.sh andreypopp/mlx 0.1
 ./scripts/import.sh andreypopp/mlx 0.2
+./scripts/import.sh andreypopp/mlx 0.3
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.1
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.2
+./scripts/import.sh andreypopp/ocamlformat-mlx 0.3
