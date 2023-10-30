da7595f1d64ef2ae47229493adcd77dd4d41ac0d
Andrey Popp <8mayday@gmail.com>
2023-10-30 22:09:27 +0400

    mlx.0.5

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -2,8 +2,10 @@
 ./scripts/import.sh andreypopp/mlx 0.2
 ./scripts/import.sh andreypopp/mlx 0.3
 ./scripts/import.sh andreypopp/mlx 0.4
+./scripts/import.sh andreypopp/mlx 0.5
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.1
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.2
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.3
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.4
+./scripts/import.sh andreypopp/ocamlformat-mlx 0.5
 ./scripts/import.sh andreypopp/dune "3.11.0+merlin-reader"
