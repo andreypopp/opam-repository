9317f8621ef348a6230721fab186c845b6bfbc2f
Andrey Popp <8mayday@gmail.com>
2024-04-19 17:51:11 +0300

    ppx_router.0.2

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -16,3 +16,4 @@
 ./scripts/import.sh andreypopp/ppx_deriving_json 0.1
 ./scripts/import.sh andreypopp/ppx_deriving_json 0.2
 ./scripts/import.sh andreypopp/ppx_router 0.1
+./scripts/import.sh andreypopp/ppx_router 0.2
