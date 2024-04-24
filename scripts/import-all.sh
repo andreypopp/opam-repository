1d98d7fa660f0791180bfe1bfd89a00796d7cc14
Andrey Popp <8mayday@gmail.com>
2024-04-24 23:51:25 +0400

    ppx_deriving_router.0.4

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -18,3 +18,4 @@
 ./scripts/import.sh andreypopp/ppx_router 0.1
 ./scripts/import.sh andreypopp/ppx_router 0.2
 ./scripts/import.sh andreypopp/ppx_router 0.3
+./scripts/import.sh andreypopp/ppx_deriving_router 0.4
