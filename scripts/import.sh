86bf0e2b2d6ef0d7a4e4676372cd8812648cea48
Andrey Popp <8mayday@gmail.com>
2024-04-27 21:07:24 +0400

    +react_browser.0.1 +react_server.0.1 +remote.0.1 +realm.0.1

--- scripts/import.sh
+++ scripts/import.sh
@@ -7,14 +7,17 @@ urlencode() {
 import() {
   local repo="$1";
   local release="$2";
-  local files=$(gh api "repos/$repo/contents" --jq '.[]|.name' | rg '.opam$')
-  echo "$files" | while read file; do
-    local name="${file%.opam}"
+  local opam_files="$3";
+  if [ -z "$opam_files" ]; then
+    opam_files=$(gh api "repos/$repo/contents" --jq '.[]|.name' | rg '.opam$')
+  fi
+  echo "$opam_files" | while read file; do
+    local name="$(basename "${file%.opam}")"
     local pkg_dir="packages/$name/$name.$release"
     local pkg_opam="$pkg_dir/opam"
     local pkg_url="$pkg_dir/url"
     mkdir -p "$pkg_dir"
-    gh api "repos/$repo/contents/$name.opam?ref=$(urlencode "$release")" --jq '.content' | base64 --decode > "$pkg_opam"
+    gh api "repos/$repo/contents/$file?ref=$(urlencode "$release")" --jq '.content' | base64 --decode > "$pkg_opam"
     echo "version: \"$release\"" >> "$pkg_opam"
     local tarball=$(gh release view -R "$repo" "$release" --json tarballUrl --jq .tarballUrl)
     echo "archive: \"$tarball\"" > "$pkg_url"
