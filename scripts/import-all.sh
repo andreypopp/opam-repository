0e5e10e4aced66ef42f6992ad15b49fa46f420fc
Andrey Popp <8mayday@gmail.com>
2024-05-05 13:51:47 +0400

    update

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -1,9 +1,4 @@
-./scripts/import.sh andreypopp/mlx 0.1
-./scripts/import.sh andreypopp/mlx 0.2
-./scripts/import.sh andreypopp/mlx 0.3
-./scripts/import.sh andreypopp/mlx 0.4
-./scripts/import.sh andreypopp/mlx 0.5
-./scripts/import.sh andreypopp/mlx 0.6
+./scripts/import.sh ocaml-mlx/mlx 0.7
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.1
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.2
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.3
