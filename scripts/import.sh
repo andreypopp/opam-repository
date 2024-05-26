99835828cf7060c81065f2d3a11a224639f0b4d8
Andrey Popp <8mayday@gmail.com>
2024-05-26 17:59:37 +0400

    store url/checksum in a single opam file

--- scripts/import.sh
+++ scripts/import.sh
@@ -4,6 +4,18 @@ urlencode() {
   echo "$1" | jq -Rr @uri
 }
 
+tarball_sha512() {
+  local repo="$1"
+  local release="$2"
+  curl -SsL "$(tarball_url "$repo" "$release")" | sha512sum | cut -d' ' -f1
+}
+
+tarball_url() {
+  local repo="$1"
+  local release="$2"
+  gh release view -R "$repo" "$release" --json tarballUrl --jq .tarballUrl
+}
+
 import() {
   local repo="$1";
   local release="$2";
@@ -13,14 +25,18 @@ import() {
   fi
   echo "$opam_files" | while read file; do
     local name="$(basename "${file%.opam}")"
+    echo "importing package $name.$release from $repo"
     local pkg_dir="packages/$name/$name.$release"
     local pkg_opam="$pkg_dir/opam"
     local pkg_url="$pkg_dir/url"
     mkdir -p "$pkg_dir"
     gh api "repos/$repo/contents/$file?ref=$(urlencode "$release")" --jq '.content' | base64 --decode > "$pkg_opam"
     echo "version: \"$release\"" >> "$pkg_opam"
-    local tarball=$(gh release view -R "$repo" "$release" --json tarballUrl --jq .tarballUrl)
-    echo "archive: \"$tarball\"" > "$pkg_url"
+    echo "importing package $name.$release from $repo: computing tarball checksum"
+    local tarball=$(tarball_url "$repo" "$release")
+    local tarball_shasum=$(tarball_sha512 "$repo" "$release")
+    echo -e "url {\n  archive: \"$tarball\"\n  checksum: [ \"sha512=$tarball_shasum\" ]\n}" >> "$pkg_opam"
+    echo "importing package $name.$release from $repo: done"
   done
 }
 
