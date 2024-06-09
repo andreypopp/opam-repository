6b95f974ba66c06a0b30791d07ec785c70c16adb
Andrey Popp <8mayday@gmail.com>
2024-06-09 17:34:10 +0400

    +ppx_deriving_router.0.8

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -2,6 +2,5 @@
 ./scripts/import.sh ocaml-mlx/mlx 0.8
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.5
 ./scripts/import.sh andreypopp/ppx_deriving 0.3
-./scripts/import.sh andreypopp/ppx_deriving_router 0.6
-./scripts/import.sh andreypopp/ppx_deriving_router 0.7
+./scripts/import.sh andreypopp/ppx_deriving_router 0.8
 ./scripts/import.sh andreypopp/reactor 0.2
