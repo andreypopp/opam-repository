c4a91ab2411a6a660239f957f6d79e57e822dd7d
Andrey Popp <8mayday@gmail.com>
2024-06-09 22:05:44 +0400

    update reactor,ppx_deriving_router

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -2,5 +2,4 @@
 ./scripts/import.sh andreypopp/ocamlformat-mlx 0.5
 ./scripts/import.sh andreypopp/ppx_deriving 0.3
 ./scripts/import.sh andreypopp/ppx_deriving_router 0.8
-./scripts/import.sh andreypopp/reactor 0.2
 ./scripts/import.sh andreypopp/reactor 0.3
